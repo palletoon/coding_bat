ca5f0c6c3cec2e932ff2efe48ea346e931644034
palletoon <palletoon@gmail.com>
2021-02-21 16:23:07 -0500

    sleep_in tests written

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# coding_bat
-Recording my progress working through exercises in Python from Coding Bat https://codingbat.com/python
+# Coding Bat Python Practice
+In this repository,  I'm recording my progress working through exercises in Python from [Coding Bat] (https://codingbat.com/python). Each of the section of Coding Bat will have its own subfolder, and in each subfolder, there will be a folder for each exercise.
+
+## Completed So Far
+
+* Warmup-1
+    *sleep_in
---
+++ warmup-1/sleep_in/sleep_in_test.py
@@ -0,0 +1,21 @@
+'''
+Tests for the sleep_in fuction
+'''
+
+# import the code to be tested
+from sleep_in import sleep_in
+
+# begin writing tests
+def describe_a_sleep_in_fuction():
+
+    def that_determines_whether_or_not_we_should_sleep_in():
+        '''
+        The two things we care about are if it's a weekend/weekday,
+        and if we're on vacation or not. 2 variables that can have
+        2 values each, means we have a total of (2 x 2 = 4) 4 possible
+        outcomes to be concerned about.
+        '''
+        assert sleep_in(True, True) = True # weekday but on vacation ==> sleep in
+        assert sleep_in(True, False) = True # weekday and NOT on vacation ==> do NOT sleep in
+        assert sleep_in(False, True) = True # weekend and on vacation ==> sleep in
+        assert sleep_in(False, False) = True # weekend but NOT on vacation ==> sleep in
